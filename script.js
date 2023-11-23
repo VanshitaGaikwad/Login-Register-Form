c268630e9520bda2f503e6c510bc15fe016ae1c1
Vanshita S. Gaikwad <vanshitagaikwad96@gmail.com>
2023-11-23 13:55:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ registerButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
+
 loginButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
